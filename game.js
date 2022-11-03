88c8428707296eb310b4a0ff4d02ecd75b033e74
michael-kestler <michael.s.kestler@gmail.com>
2022-11-02 20:26:04 -0700

    used jquery to create button flash effect. added js for button sounds.

--- game.js
+++ game.js
@@ -12,6 +12,16 @@ function nextSequence(){
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   let randomChosenColor = buttonColors[randomNumber];
  gamePattern.push(randomChosenColor);
- return gamePattern;
+
+ //1. Use jQuery to select the button with the same id as the randomChosenColour
+ //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+   audio.play();
 
 }
+
+});
