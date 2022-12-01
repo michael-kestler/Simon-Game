21fcfb2013460e72c6e7d8e3546da5cc6fde675a
michael-kestler <michael.s.kestler@gmail.com>
2022-12-01 00:08:46 -0800

    added function for checking pattern and advancing levels

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ let started = false;
 
 let level = 0;
 
-$(document).keypress(function(event) {
+$(document).keypress(function() {
   if(!started) {
         // do something
         $("#level-title").text("Level " + level);
@@ -41,8 +41,9 @@ $( ".btn" ).click(function() {
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
   animatePress(userChosenColor);
+  checkAnswer(userClickedPattern);
 
-  // console.log(userClickedPattern);
+  console.log(userClickedPattern);
 });
 
 
@@ -52,15 +53,14 @@ function nextSequence(){
   console.log(level);
   $("#level-title").text("Level " + level);
 
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
-
    playSound(randomChosenColor);
-   animatePress(randomChosenColor);
+   // animatePress(randomChosenColor);
+   console.log(gamePattern);
 
 
 }
@@ -75,10 +75,29 @@ function playSound(name){
 
 function animatePress(currentColor){
 
-  $("." + currentColor).addClass('pressed');
+  $("#" + currentColor).addClass('pressed');
 
   setTimeout(function(){
-    $("." + currentColor).removeClass('pressed');
+    $("#" + currentColor).removeClass('pressed');
   }, 100);
+
+}
+
+function checkAnswer(currentLevel){
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    console.log("success");
+
+if(userClickedPattern.length === gamePattern.length){
+
+  setTimeout(function (){
+    nextSequence()
+  } , 1000);
+}
+
+
+
+  } else {
+    console.log("wrong");
+  }
 
 }
